2f9bb5494fd007620dc341cdb4f1607ec49c87f0
Tanin Rahman <taninr87@gmail.com>
2022-11-08 23:49:39 +0600

    add home service section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,16 +4,16 @@ import img from '../../../assets/banner.png';
 const Banner = () => {
   return (
     <div className='bg-gray-100'>
-      <div className='flex justify-center h-[550px] w-5/6 mx-auto'>
-      <div className='flex flex-col items-center w-[40%] justify-center'>
-        <h1 className='text-5xl font-bold'>MAKE ME GROW YOUR SKILLS,CHANGE YOUR LIFE</h1>
+      <div className='flex h-[420px] w-5/6 mx-auto'>
+      <div className='flex flex-col justify-center ml-10 items-center w-[40%]'>
+        <h1 className='text-4xl font-bold'>MAKE ME GROW YOUR SKILLS,CHANGE YOUR LIFE</h1>
         <div className='mt-10'>
           <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium text-sm px-5 py-2.5 mr-8 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Contact Me</button>
           <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">About Me</button>
         </div>
       </div>
-      <div className='flex items-end ml-5'>
-        <img className='' src={img} alt="teacherphoto" />
+      <div className='flex items-end w-full'>
+        <img src={img} alt="teacherphoto" />
       </div>
       </div>
     </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Service from "./Service";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect( () => {
+    fetch('http://localhost:5000/services')
+    .then(res => res.json())
+    .then(data => setServices(data))
+    .catch(error => console.error(error))
+  }, [])
+  
   return (
-    <div>
-      This is Services SEctiojn
+    <div className="my-10">
+      <div className="text-center">
+        <p>My Services</p>
+        <h1 className="text-4xl font-bold">What I Do?</h1>
+        <div className="h-1 w-10 bg-red-500 mx-auto mt-4"></div>
+      </div>
+      <div className='grid grid-cols-3 gap-5 w-5/6 mx-auto mt-10'>
+        {
+          services.map(service => <Service key={service._id} service={service}></Service>)
+        }
+      </div>
+      
+        <div className="mt-5 flex justify-center">
+        <Link to='/services' className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium text-sm px-5 py-2.5 dark:focus:ring-yellow-900">See All Services</Link>
+        </div>
+
     </div>
   );
 };
